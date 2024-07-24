ff9daa0faa4d6b563ae8a2990decdc743dae38ee
ZandorVillarreal <z_v_h@live.com>
2024-07-23 19:53:55 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string firstName = "John";
+    std::string lastName = "Doe";
+    std::string streetAddress = "123 Elm Street";
+    std::string city = "Anytown";
+    std::string zipCode = "12345";
+
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
